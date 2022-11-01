40d62f1c8ad47042ece7393fb82ae0953eedea4d
Vinay Jayaram <jayaram.vinay@gmail.com>
2022-11-01 11:32:02 +0530

    share code and contact us updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 var hamburger = document.querySelector(".hamb");
 var navlist = document.querySelector(".nav-list");
 var links = document.querySelector(".nav-list li");
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
 
 hamburger.addEventListener("click", function(){
     this.classList.toggle("click");
     navlist.classList.toggle("open");
+});
+
+
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'With pride and joy, I cordially invite you to share a special day in my life as i am given a name on 10th November at 10AM',
+      url: 'https://maps.app.goo.gl/KUUTWYXDGRAs6qyZ8'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        shareDialog.classList.add('is-open');
+    }
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
 });
